1f462ebedc122d355db89c5b7ea70d51b06b8d0a
hirotakasengoku999 <hirotakasengoku@gmail.com>
2022-03-18 10:08:13 +0900

    Modifiled ssg-ssr.md, profile.jpeg index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>僕は仙石裕貴です。3人の父親。年収アップの為猛勉強するぞ</p>
+        <p>僕は仙石裕貴です。3児のパパ。沢山稼げるようになりたい</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
